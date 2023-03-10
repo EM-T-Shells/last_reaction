cfa7d8a2411ba8e0b02ead6f0888af0c0b7465d4
unknown <taskaleg@gmail.com>
2023-03-10 13:07:06 -0600

    updated ss

--- portfolio/App.js
+++ portfolio/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./components/Home";
 import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
+import Portfolio from "./pages/Project";
 import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
 
--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import About from "../pages/About";
-import Portfolio from "../pages/Portfolio";
+import Portfolio from "../pages/Project";
 import Contact from "../pages/Contact";
 import Resume from "../pages/Resume";
 
