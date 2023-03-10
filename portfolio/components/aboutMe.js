67be4807e6444d7cfb5bc4842eab13ccadd5b5ed
unknown <taskaleg@gmail.com>
2023-03-10 10:41:18 -0600

    wrote all about me ;)

---
+++ portfolio/components/aboutMe.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <div>
+        <img src="profileimg.jpg" alt="profile img" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+          necessitatibus earum suscipit ut, soluta cumque sint, corporis
+          molestiae adipisci, ipsam quaerat eveniet exercitationem maiores porro
+          quam obcaecati facilis. Accusamus!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
